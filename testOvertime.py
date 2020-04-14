5a92ec9449ac38ad7e1c35fb8d0a5db5090e2fdd
SuperWaterGod <56631400+SuperWaterGod@users.noreply.github.com>
2020-04-14 18:50:38 +0800

    Delete testOvertime.py

--- testOvertime.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-with open('./overtime.csv', encoding='gb18030') as f:
-    data = np.loadtxt(f, str, delimiter=",", usecols=(0, 0))
-a = 0.0
-for i in range(0, len(data[:-1, 0])):
-    print(data[i, 0])
-print("超时一共：")
-print(len(data[:-1, 0]) + 1)
